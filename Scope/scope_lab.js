bfe44499c1ddea6dcdf3d4a07c2cf8dcb0cd71f6
SpeedyM28 <jamil.bitarm76@gmail.com>
2024-10-23 05:36:29 -0400

    updating repo with course work

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -1,2 +1,30 @@
 // Global scope
 var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+{
+    //Block scope
+    var blockVar = "I'm a block-scoped var";
+    var blockLet = "I'm a block-scoped let";
+    var blockConst = "I'm a block-scoped const";
+}
+
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst);
+
+//console.log(blockVar);
+/console.log(blockLet);
+
+
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const"
+}
+show();
+
+console.log(functionVar);
+console.log(functionLet);
+console.log(functionConst);
