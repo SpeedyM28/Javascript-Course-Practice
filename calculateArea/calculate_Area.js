3e323c5b47bdb0351d36c641dbfea6de4a0f0974
SpeedyM28 <jamil.bitarm76@gmail.com>
2024-10-23 10:24:05 -0400

    extra practice

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,26 @@ function calculateArea(){
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function calculate_total_expenditure(){
+    let a = parseFloat(document.getElementById('grocery1').value);
+    let b = parseFloat(document.getElementById('grocery2').value);
+    let c = parseFloat(document.getElementById('grocery3').value);
+
+    let total = a+b+c;
+
+    //note, the next 6 lines are due to a small
+    //javascript error which would result in 
+    //calculations such as:
+    //3+2+5.12 = 10.120000000001
+    //hence to eliminate that inaccuracy the code
+    //below was produced
+    let fix = total * 100;
+    if(fix != parseInt(fix)){
+        fix = parseInt(fix);
+        fix = parseFloat(fix / 100);
+        total = fix;
+    }
+
+    document.getElementById('grocery_result').innerText = `The total amount is: $${total}`
+}
