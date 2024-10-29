562ba96b7047e499671e46706307d067ea2e05fc
SpeedyM28 <jamil.bitarm76@gmail.com>
2024-10-29 09:58:05 -0400

    had nothing to do with being activated, that was physically painful

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,20 +1,25 @@
 function showweatherDetails(event) {
     event.preventDefault();
+
+    const city = document.getElementById('city').value;
+    const apiKey = '562d7f14c451a99c473b2f6da267d0c0'; // Replace 'YOUR_API_KEY' with your actual API key
+    const cityapiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(cityapiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data["name"]}</h2>
+                                    <p>Temperature: ${data.main.temp} &#8451;</p>
+                                    <p>Weather: ${data.weather[0].description}</p>`;
+        })
 }
 
-const city = document.getElementById('city').value;
-const apiKey = '562d7f14c451a99c473b2f6da267d0c0'; // Replace 'YOUR_API_KEY' with your actual API key
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+// const city = document.getElementById('city').value;
+// const long = document.getElementById('long').value;
+// const lat = document.getElementById('lat').value;
+// const apiKey = '562d7f14c451a99c473b2f6da267d0c0'; // Replace 'YOUR_API_KEY' with your actual API key
+// const cityapiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+// const longapiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
 
-//THIS WAS LAST TESTED 1:00PM UTC, API KEY WAS NOT ACTIVE hence request failed
-
-fetch(apiUrl)
-.then(response => response.json())
-.then(data => {
-  const weatherInfo = document.getElementById('weatherInfo');
-  weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
-                          <p>Temperature: ${data.main.temp} &#8451;</p>
-                          <p>Weather: ${data.weather[0].description}</p>`;
-})
-
-document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails);
